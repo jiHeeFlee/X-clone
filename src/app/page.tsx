b193a95ed2c51f1fa932142848f00df6e655e18a
jiHeeFlee <esder1310@naver.com>
2025-02-06 16:57:34 +0900

    style : 퍼블리싱 진행

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,20 @@
-import Image from "next/image";
 import styles from "./page.module.css";
+import Image from "next/image";
 import Link from "next/link";
 import jLogo from '../../public/logo.svg';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.container}>
       <div className={styles.left}>
-        <Image src={jLogo} alt="logo" />
+        <Image src={jLogo} alt="logo" style={{width:'300px', height:'300px'}}/>
       </div>
-      <div>
+      <div className={styles.right}>
         <h1>지금 일어나고 있는 일</h1>
         <h2>지금 가입하세요.</h2>
-        <Link href='/i/flow/signup'/>
+        <Link href='/i/flow/signup' className={styles.signup}>계정 만들기</Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
+        <Link href='/login' className={styles.login}>로그인</Link>
       </div>
     </div>
   );
