5d100fd5109fe54bec533ef0f619982386e94cf2
jiHeeFlee <esder1310@naver.com>
2025-02-06 16:16:10 +0900

    style : 예시 코드 return 작성

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
+import jLogo from '../../public/logo.svg';
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      app/홈 페이지
+    <div>
+      <div className={styles.left}>
+        <Image src={jLogo} alt="logo" />
+      </div>
+      <div>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요.</h2>
+        <Link href='/i/flow/signup'/>
+      </div>
     </div>
   );
 }
