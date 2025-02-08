e46d8f534ebe17fe3d6d062c716de90efb4ad260
jiHeeFlee <esder1310@naver.com>
2025-02-08 19:08:33 +0900

    add : intercepting route를 위한 파일 추가

--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -8,17 +8,17 @@ type Props = {
 export default function Layout({children,modal}:Props){
     return(
         <div className={styles.container}>
-            비포로그인 레이아웃
             {children}
             {modal}
         </div>
     )
 }
 
-// 주소가 localhost:3001일때는
-// children -> page.tsx
-// modal -> @modal/default.tsx
+// [ default.tsx ]
+// 1. 주소가 localhost:3001일때는
+// children -> page.tsx 
+// modal -> @modal/default.tsx 
 
-// 주소가 localhost:3001/i/flow/login 때는
-// children -> i/flow/login/page.tsx
-// modal -> @modal/i/flow/login/page.tsx
+// 2. 주소가 localhost:3001/i/flow/login 때는
+// children -> i/flow/login/page.tsx 
+// modal -> @modal/i/flow/login/page.tsx 
